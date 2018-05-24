8d2afa1a16a72d199c5f4d5d6976b11a8dd5c77e
Alex Wilson <fonzmp@gmail.com>
2018-05-24 18:29:17 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -91,7 +91,7 @@ class MusicLibraryController
     user_index = user_input.to_i - 1
     binding.pry
     if user_input != 0
-      puts "Playing #{list[user_index].name} by #{list[user_index].artist.name}.""
+      puts "Playing #{list[user_index].name} by #{list[user_index].artist.name}."
     end
   end
 
