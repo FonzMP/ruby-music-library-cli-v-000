90daf9202ec021773032b1703032577b05d20be1
Alex Wilson <fonzmp@gmail.com>
2018-05-24 18:37:46 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -91,7 +91,7 @@ class MusicLibraryController
     total_length = Song.all.length
     # binding.pry
     if user_index.between?(1, total_length)
-      binding.pry
+      Song.all.sort
       if user_input >= 1 || user_input < total_length
         puts "Playing #{list[user_index].name} by #{list[user_index].artist.name}."
       end
